81b2bcd7ef85c38cf43c6ed3ff4b958fc6e743b0
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-04-04 04:55:54 +0800

    updated lib usage

--- src/motordriver/MotorDriver.scala
+++ src/motordriver/MotorDriver.scala
@@ -14,20 +14,23 @@ import scala.collection.mutable.ArrayBuffer
  */
 class MotorDriver extends Thread {
 
-  class CommandPair(val direction: Double, val left: Double, val right: Double)
-
+  /**
+   * input: direction (range from 0 to 1)
+   * output: left & right motor pwm (range from 0 to 1)
+   */
+  val ai = new NeuroEvolution(n_Bit_Weight = 3, n_Bit_Bias = 3, numberOfNodes = Array(1, 8, 16, 8, 2), activationFunction = TweakedCosine,
+    popSize = 16, pSelection = 0.25, pMutationPow = 8, aMutationPow = 4, parent_immutable = true,
+    get_perceptron_inputs = get_perceptron_inputs, eval_perceptron_function = eval_perceptron_function,
+    problemType = Minimize,
+    diversityWeight = 0.1,
+    loopInterval = 0)
   //(direction,left,right)
   var sampleCommandPairs = new ArrayBuffer[CommandPair]
   setup
 
-  def reScale(double: Double) = {
-    (double + 1) / 2d
-  }
-
   def reversedScale(double: Double) = {
     double * 2d - 1
   }
-
 
   def setup = {
     sampleCommandPairs += new CommandPair(-1d, -1d, -1d)
@@ -41,22 +44,14 @@ class MotorDriver extends Thread {
     sampleCommandPairs += new CommandPair(1d, -1d, -1d)
   }
 
-
-  /**
-   * input: direction (range from -1 to 1)
-   * output: left & right motor pwm (range from -1 to 1)
-   */
-  val ai = new NeuroEvolution(n_Bit_Weight = 8, n_Bit_Bias = 8, numberOfNodes = Array(1, 8, 16, 8, 2), activationFunction = TweakedCosine,
-    popSize = 100, pSelection = 0.25, pMutation = 0.1, aMutation = 0.03, parent_immutable = true,
-    get_perceptron_inputs = get_perceptron_inputs, eval_perceptron_function = eval_perceptron_function,
-    problemType = Minimize,
-    diversityWeight = 0.1,
-    loopInterval = 0)
-
   def get_perceptron_inputs: Array[Array[Double]] = {
     var inputs = ArrayBuffer.empty[Array[Double]]
     sampleCommandPairs.foreach(commandPair => inputs += Array(reScale(commandPair.direction)))
     inputs.toArray
+  }
+
+  def reScale(double: Double) = {
+    (double + 1) / 2d
   }
 
   def eval_perceptron_function(inputs: Array[Double], outputs: Array[Double]): Double = {
@@ -70,15 +65,28 @@ class MotorDriver extends Thread {
   override def run = {
     ai.start()
     //val log=new ObjectOutputStream(new FileOutputStream("MotorDriver.perceptron"))
+    var nowTime: Long = 0
+    var lastTime: Long = nowTime
+    var nowRound: Int = -1
+    var lastRound: Int = nowRound
+    var fileCount = 1
+    var roundPerSecond = 0d
+    val alpha = 0.125
     while (true) {
+      lastTime = nowTime
+      nowTime = System.currentTimeMillis()
+      lastRound = nowRound
       val best = ai.ga.getBestGene
-      var message = "\nRound: " + ai.ga.round
-      message += "\nBest's fitness: " + Math.sqrt(best.getFitness / 2d / sampleCommandPairs.length)
-      message += "\nBest's diversity: " + best.diversity
-      message += "\nOverall diversity: " + (1 - ai.ga.diversity_weight)
+      nowRound = ai.ga.round
+      roundPerSecond = roundPerSecond * (1 - alpha) + alpha * (nowRound - lastRound) * 1000 / (nowTime - lastTime)
+      var message = "\nRound: \t" + nowRound + "\t(" + Math.round(roundPerSecond) + " rounds per Second)"
+      message += "\nBest's fitness: \t" + Math.sqrt(best.getFitness / 2d / sampleCommandPairs.length)
+      //message += "\nBest's diversity: " + best.diversity
+      message += "\nOverall diversity: \t" + (1 - ai.ga.overallDiversity)
       println(message)
       //log.writeObject(best.rawCode)
-      printToFile(new File(ai.ga.round % 2 + "-MotorDriver.perceptron")) { p =>
+      fileCount = (fileCount + 1) % 2
+      printToFile(new File(fileCount + "-MotorDriver.perceptron")) { p =>
         best.rawCode.foreach(b =>
           if (b) p.print(1)
           else p.print(0)
@@ -97,5 +105,7 @@ class MotorDriver extends Thread {
       p.close()
     }
   }
+
+  class CommandPair(val direction: Double, val left: Double, val right: Double)
 }
 
--- src/motordriver/Utils.scala
+++ src/motordriver/Utils.scala
@@ -6,5 +6,5 @@ import scala.util.Random
  * Created by beenotung on 4/3/15.
  */
 object Utils {
-val random=new Random(System.currentTimeMillis())
+  val random = new Random(System.currentTimeMillis())
 }

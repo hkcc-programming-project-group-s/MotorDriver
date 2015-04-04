1e461373a2561d1831f2d5fcd9635f95ee74eb46
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-04-04 14:03:03 +0800

    to add [load from file feature]

--- src/motordriver/MotorDriver.scala
+++ src/motordriver/MotorDriver.scala
@@ -18,14 +18,16 @@ class MotorDriver extends Thread {
    * input: direction (range from 0 to 1)
    * output: left & right motor pwm (range from 0 to 1)
    */
-  val ai = new NeuroEvolution(n_Bit_Weight = 3, n_Bit_Bias = 3, numberOfNodes = Array(1, 8, 16, 8, 2), activationFunction = TweakedCosine,
-    popSize = 16, pSelection = 0.25, pMutationPow = 8, aMutationPow = 4, parent_immutable = true,
+  val ai = new NeuroEvolution(n_Bit_Weight = 4, n_Bit_Bias = 4, numberOfNodes = Array(1, 4, 8, 4, 2), activationFunction = TweakedCosine,
+    popSize = 8, pSelection = 0.25, pMutationPow = 2, aMutationPow = 4, parent_immutable = true,
     get_perceptron_inputs = get_perceptron_inputs, eval_perceptron_function = eval_perceptron_function,
     problemType = Minimize,
     diversityWeight = 0.1,
     loopInterval = 0)
+
   //(direction,left,right)
   var sampleCommandPairs = new ArrayBuffer[CommandPair]
+
   setup
 
   def reversedScale(double: Double) = {
@@ -67,27 +69,34 @@ class MotorDriver extends Thread {
     //val log=new ObjectOutputStream(new FileOutputStream("MotorDriver.perceptron"))
     var nowTime: Long = 0
     var lastTime: Long = nowTime
-    var nowRound: Int = -1
+    var nowRound: Int = -200
     var lastRound: Int = nowRound
     var fileCount = 1
     var roundPerSecond = 0d
     val alpha = 0.125
+    var nowBest = ai.ga.getBestGene
+    var lastBest = nowBest
+    var estimatedRoundLeft = 100d
     while (true) {
       lastTime = nowTime
       nowTime = System.currentTimeMillis()
       lastRound = nowRound
-      val best = ai.ga.getBestGene
       nowRound = ai.ga.round
-      roundPerSecond = roundPerSecond * (1 - alpha) + alpha * (nowRound - lastRound) * 1000 / (nowTime - lastTime)
-      var message = "\nRound: \t" + nowRound + "\t(" + Math.round(roundPerSecond) + " rounds per Second)"
-      message += "\nBest's fitness: \t" + Math.sqrt(best.getFitness / 2d / sampleCommandPairs.length)
-      //message += "\nBest's diversity: " + best.diversity
+      lastBest = nowBest
+      nowBest = ai.ga.getBestGene
+      roundPerSecond = roundPerSecond * (1 - alpha) + alpha * (nowRound - lastRound) * 1000d / (nowTime - lastTime)
+      var message = "\nRound: \t" + nowRound
+      message += "\n" + Math.round(roundPerSecond) + " rounds per Second"
+      message += "\nBest's fitness: \t" + Math.sqrt(nowBest.getFitness / 2d / sampleCommandPairs.length)
       message += "\nOverall diversity: \t" + (1 - ai.ga.overallDiversity)
+      message += "\nImprovement: \t\t" + Math.round((nowBest.fitness - lastBest.fitness) / lastBest.fitness * 100) / 100d + "%"
+      estimatedRoundLeft = estimatedRoundLeft * (1 - alpha) + alpha * Math.abs(nowBest.fitness / ((lastBest.fitness - nowBest.fitness + 0.00001) / (lastTime - nowTime)) * roundPerSecond)
+      message += "\nestimate " + estimatedRoundLeft + " round left"
       println(message)
       //log.writeObject(best.rawCode)
       fileCount = (fileCount + 1) % 2
       printToFile(new File(fileCount + "-MotorDriver.perceptron")) { p =>
-        best.rawCode.foreach(b =>
+        nowBest.rawCode.foreach(b =>
           if (b) p.print(1)
           else p.print(0)
         )
@@ -107,5 +116,6 @@ class MotorDriver extends Thread {
   }
 
   class CommandPair(val direction: Double, val left: Double, val right: Double)
+
 }
 
